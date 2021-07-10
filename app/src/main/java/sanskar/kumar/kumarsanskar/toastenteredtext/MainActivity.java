369f6b064ae638c127869b3e8f751789b16164bf
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-10 21:34:01 +0530

    intialized the enterText and pressHere objects

--- app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
@@ -14,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterText = (EditText) findViewById(R.id.editText);
+        pressHere = (Button) findViewById(R.id.button);
+
     }
 }
