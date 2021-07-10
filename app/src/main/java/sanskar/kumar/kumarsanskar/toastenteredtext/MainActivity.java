877d88a2f052ed7e87cb26434f6bb5ef5787b65c
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-10 21:35:21 +0530

    called the setOnClickListener() method with the press here button

--- app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
@@ -3,6 +3,7 @@ package sanskar.kumar.kumarsanskar.toastenteredtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         enterText = (EditText) findViewById(R.id.editText);
         pressHere = (Button) findViewById(R.id.button);
+        pressHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
