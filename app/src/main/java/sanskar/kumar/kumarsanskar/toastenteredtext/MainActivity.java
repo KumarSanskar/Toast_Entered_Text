cea95448cf65c542f9fd8436117952a82caa23c5
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-10 21:32:18 +0530

    add object for class Button and EditText

--- app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
@@ -3,8 +3,12 @@ package sanskar.kumar.kumarsanskar.toastenteredtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText enterText;
+    Button pressHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
