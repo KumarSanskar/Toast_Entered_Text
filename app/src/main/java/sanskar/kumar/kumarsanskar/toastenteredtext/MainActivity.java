9236dc435f5f6a5aa116d927cc01ff7497690f1b
Kumar Sanskar <kumar.sanskar_cs19@gla.ac.in>
2021-07-10 22:24:58 +0530

    optimized the imports

--- app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
+++ app/src/main/java/sanskar/kumar/kumarsanskar/toastenteredtext/MainActivity.java
@@ -1,12 +1,12 @@
 package sanskar.kumar.kumarsanskar.toastenteredtext;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText enterText;
